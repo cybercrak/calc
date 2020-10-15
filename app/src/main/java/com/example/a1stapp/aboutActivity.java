db8c5f1d6d86030bca86080d6d38e01eca9034c4
cybercrak/calc <nsarran37@gmail.com>
2020-10-16 00:27:08 +0530

    added navigation activity

---
+++ app/src/main/java/com/example/a1stapp/aboutActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1stapp;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class aboutActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+}
